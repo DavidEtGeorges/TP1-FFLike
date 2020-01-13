b310e6a91ae3e7c887baf876d8d1683033ea629a
DavidEtGeorges <59653406+DavidEtGeorges@users.noreply.github.com>
2020-01-13 12:12:52 +0100

    Create FFLike.c

---
+++ FFLike.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(){
+int pointsDeVieMonstre = 1;
+int pointsDeVieHeros = 1;
+int pMMonstre = 0;
+int pMHeros = 0;
+int action = 0;
+int jaugePoisonHeros
+int jaugePoisonMonstre
+
+while(pointsDeVieMonstre && pointsDeVieHeros != 0){
+printf("------------------------------\n");
+printf(" |Valefore : %d Points de vie ",pointsDeVieMonstre);
+printf("%d PM\n",pMMonstre);
+printf("Vous : %d Points de vie ",pointsDeVieHeros);
+printf("%d PM\n",pMHeros);
+printf("------------------------------\n");
+printf("C'est a vous d'agir !\n 1 -Attaquer(10)\n 2 -Defense(Attaque adverses divisees par 4)\n 3 -Poison(3PM)\n");
+		while(action != 1 || action != 2 || action !=3){
+			scanf(%d,&action);
+}
+		if(action == 1){
+			
+		}
+		if(action == 2){
+			
+		}
+		if(action == 3){
+			
+		}
+
+
+
+
+pointsDeVieMonstre--;
+
+}
+printf("Perdu !");
+
+
+
+
+
+
+return 0;
+}
