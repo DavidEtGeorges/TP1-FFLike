a94a3bf05a0dd74d141290f91dd142c712ce2749
DavidEtGeorges <59653406+DavidEtGeorges@users.noreply.github.com>
2020-01-13 12:39:43 +0100

    Update FFLike.c

--- FFLike.c
+++ FFLike.c
@@ -6,11 +6,11 @@ int pointsDeVieHeros = 1;
 int pMMonstre = 0;
 int pMHeros = 0;
 int action = 0;
-int jaugePoisonHeros
-int jaugePoisonMonstre
-int protection = 0
+int jaugePoisonHeros;
+int jaugePoisonMonstre;
+int protection = 0;
 
-while(pointsDeVieMonstre && pointsDeVieHeros != 0){
+while(pointsDeVieMonstre > 0 && pointsDeVieHeros > 0){
 printf("------------------------------\n");
 printf(" |Valefore : %d Points de vie ",pointsDeVieMonstre);
 printf("%d PM\n",pMMonstre);
@@ -18,9 +18,9 @@ printf("Vous : %d Points de vie ",pointsDeVieHeros);
 printf("%d PM\n",pMHeros);
 printf("------------------------------\n");
 printf("C'est a vous d'agir !\n 1 -Attaquer(10)\n 2 -Defense(Attaque adverses divisees par 4)\n 3 -Poison(3PM)\n");
-		while(action != 1 || action != 2 || action !=3){
-			scanf(%d,&action);
-}
+		do{
+			scanf("%d",&action);
+		}while(action < 1 && action > 3);
 		if(action == 1){
 			pointsDeVieMonstre = pointsDeVieMonstre-10;
 			printf("Le monstre a perdu 10 PV !\n Il lui reste %d",pointsDeVieMonstre);
@@ -34,15 +34,13 @@ printf("C'est a vous d'agir !\n 1 -Attaquer(10)\n 2 -Defense(Attaque adverses di
 			pMHeros = pMHeros-3;
 			printf("Le monstre est sous-poison ! \n");
 		}
-		else{
-			printf("Vous ave zpas assez de Pm !\n");
+		if(action == 3 && pMHeros < 3){
+			printf("Vous avez pas assez de Pm !\n");
 		}
 
 
 pMHeros++;
 pMMonstre++;
-pointsDeVieMonstre--;
-
 }
 printf("Perdu !");
 
